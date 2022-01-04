99668733f960557f4191000673be7e231951a901
Cannon Lock <clock@wisc.edu>
2022-01-04 11:39:08 -0600

    nuke unused previews

--- preview-add-tickets/tickets/files/sleep.bat
+++
@@ -1,9 +0,0 @@
-@echo off & setlocal
-if x%1% == x (
-  echo Usage: "sleep <seconds>"
-  goto :EOF
-) 
-set /a COUNT=%1+1
-echo Sleeping for %1 second(s)
-ping -n %COUNT% 127.0.0.1 >NUL 2>&1
-endlocal
