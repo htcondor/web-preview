60ab57931a4fc2eaf00b61fdacdb23c7d8f3b665
Cannon Lock <clock@wisc.edu>
2022-12-19 16:25:38 -0600

    nuke

--- preview-search-bar/assets/js/default.js
+++
@@ -1,9 +0,0 @@
-// This page hosts the code that is used on every single page.
-
-// If your js function does not need to be on every page don't put it here!
-
-window.onload = () => {
-    const MainSearchBar = new SearchBar("main-search-bar",
-        "/web-preview/preview-search-bar/assets/search/main_index.json",
-        "/web-preview/preview-search-bar/assets/search/main_metadata.json")
-}
