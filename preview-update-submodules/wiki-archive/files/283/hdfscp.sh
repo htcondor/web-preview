ec7f968b4923b220ca2f5fc06d15957787189ef3
Automatic preview publish <>
2023-04-24 17:49:00 +0000

    Build Branch Website

---
+++ preview-update-submodules/wiki-archive/files/283/hdfscp.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+HDFS_HOME=$(condor_config_val HDFS_HOME)
+
+if [ HDFS_HOME="Not Defined: HDFS_HOME" ] ; then
+	HDFS_HOME=$(condor_config_val LIBEXEC)/hdfs
+fi
+#echo $HDFS_HOME
+
+CLASSPATH=$HDFS_HOME
+
+for f in $HDFS_HOME/hadoop*.jar; do
+	CLASSPATH=${CLASSPATH}:$f;
+done
+
+#"COMMAND" = "dfs" 
+CLASS=org.apache.hadoop.fs.FsShell
+
+# add libs to CLASSPATH
+for f in $HDFS_HOME/lib/*.jar; do
+	CLASSPATH=${CLASSPATH}:$f;
+done
+
+SRC=$1
+
+OPTION="-put"
+
+if [ SRC==hdfs://* ] ; then
+  OPTION="-get"
+fi
+#if $cygwin; then
+#  CLASSPATH=`cygpath -p -w "$CLASSPATH"`
+#fi
+
+#export CLASSPATH=$CLASSPATH
+
+java "-classpath" $CLASSPATH $CLASS $OPTION "$@"
+
