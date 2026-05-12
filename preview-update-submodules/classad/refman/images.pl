46e42dd15ed60e6c8731f1ca9e34551ac789095a
Automatic preview publish <>
2026-05-12 16:20:27 +0000

    Build Branch Website

---
+++ preview-update-submodules/classad/refman/images.pl
@@ -0,0 +1,127 @@
+# LaTeX2HTML 2002-2-1 (1.70)
+# Associate images original text with physical files.
+
+
+$key = q/sqsubseteq;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="16" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$\sqsubseteq$">|; 
+
+$key = q/mathit{lookup}(mathtt{c},(E_2,C))=(mathbf{undefined},mathbf{undefined});MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="324" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="$\mathit{lookup}(\mathtt{c}, (E_2, C)) = (\mathbf{undefined},
+\mathbf{undefined})$">|; 
+
+$key = q/mathit{eval}(E_i[s],C_b')=(E_i',C_i');MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="178" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img17.png"
+ ALT="$\mathit{eval}(E_i[s], C_b') = (E_i', C_i')$">|; 
+
+$key = q/mathit{lookup}(mathtt{a},(E_2,C))=(x,C);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="190" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="$\mathit{lookup}(\mathtt{a}, (E_2, C)) = (x, C)$">|; 
+
+$key = q/(E_2,C)sqsubseteq(C,C);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="120" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$(E_2, C) \sqsubseteq (C, C)$">|; 
+
+$key = q/(E,C)sqsubseteq(E_p,C);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="121" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$(E, C) \sqsubseteq (E_p, C)$">|; 
+
+$key = q/(E_1,C)sqsubseteq(R,C)sqsubseteq(C,C);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="185" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$(E_1, C) \sqsubseteq (R, C) \sqsubseteq (C, C)$">|; 
+
+$key = q/odot;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="16" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="$\odot$">|; 
+
+$key = q/(E,C)sqsubseteq(E',C);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="119" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$(E, C) \sqsubseteq (E', C)$">|; 
+
+$key = q/octal_digit^+;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="87" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$octal\_digit^+$">|; 
+
+$key = q/mathit{eval}(f(E_1,...,E_n),C)=(V,V);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="217" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img18.png"
+ ALT="$\mathit{eval}(f(E_1,...,E_n), C) = (V, V)$">|; 
+
+$key = q/(E',C')=mathit{eval}(mathit{lookup}(s,(E_b',C_b')));MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="246" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img16.png"
+ ALT="$(E', C') = \mathit{eval}(\mathit{lookup}(s, (E_b', C_b')))$">|; 
+
+$key = q/{displaymath}eval(E_1odotE_2,C)=(c,c){displaymath};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="170" HEIGHT="28" BORDER="0"
+ SRC="|."$dir".q|img15.png"
+ ALT="\begin{displaymath}eval(E_1 \odot E_2, C) = (c, c) \end{displaymath}">|; 
+
+$key = q/mathit{lookup}(mathtt{a},(E_1,C))=(y,C);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="189" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$\mathit{lookup}(\mathtt{a}, (E_1, C)) = (y, C)$">|; 
+
+$key = q/mathit{eval}(E_i,C)=E_i';MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="120" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img19.png"
+ ALT="$\mathit{eval}(E_i, C) = E_i'$">|; 
+
+$key = q/pm;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="16" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$\pm$">|; 
+
+$key = q/mathit{lookup}(mathtt{c},(E_1,C))=(E_1,C);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="199" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="$\mathit{lookup}(\mathtt{c}, (E_1, C)) = (E_1, C)$">|; 
+
+$key = q/(E,C)sqsubseteq(E_d,C);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="121" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$(E, C) \sqsubseteq (E_d, C)$">|; 
+
+$key = q/0ler<x;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="71" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img20.png"
+ ALT="$0 \le r &lt; x$">|; 
+
+$key = q/resizebox{!}{1.2in}{includegraphics{example.eps}};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="401" HEIGHT="136" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\resizebox{!}{1.2in}{\includegraphics{example.eps}}">|; 
+
+1;
+
