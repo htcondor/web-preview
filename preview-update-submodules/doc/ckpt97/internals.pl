20a3b01ce7455d8b76a029729f479ea75e7b4947
Automatic preview publish <>
2026-07-24 20:59:01 +0000

    Build Branch Website

---
+++ preview-update-submodules/doc/ckpt97/internals.pl
@@ -0,0 +1,12 @@
+# LaTeX2HTML 96.1 (Feb 5, 1996)
+# Associate internals original text with physical files.
+
+$key = q/seclimitations/;
+$ref_files{$key} = 'node16.html'; 
+$key = q/secsyscalls/;
+$ref_files{$key} = 'node11.html'; 
+$key = q/secremote/;
+$ref_files{$key} = 'node15.html'; 
+
+1;
+
